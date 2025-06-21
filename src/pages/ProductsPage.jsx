7f142a14af30dad7188db4ea4e7f4d8ab0b16d9a
SmartTableNZ <smarttablenz@gmail.com>
2025-06-21 12:39:50 +1200

    Add Photos

--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -1,6 +1,7 @@
 import logo from "../assets/images/DarkLogo.png";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import ImageCarousel from "./components/ImageCarousel";
 
 const ProductsPage = () => {
   return (
